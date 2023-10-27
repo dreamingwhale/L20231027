9fb84b041a3b09ad0a221243aca5ef8d9e7be9fa
dreamingwhale <dreamingskywhale@gmail.com>
2023-10-27 09:59:30 +0900

    20231027 수정

--- Main.cpp
+++ Main.cpp
@@ -3,25 +3,50 @@
 #include <windows.h>
 using namespace std;
 
-int main() {
+int main()
+{
 	//random함수 사용시
 	int Num1 = 0;
-	for (int i = 1; i <= 4; i++) {
-	Num1 = 0;
-	srand(time(NULL)*i);
-	Num1 = (rand() % 52);
-	cout << Num1<<endl;
+	int Array[4] = { 0,0,0,0 };
+	for (int i = 0; i < 4; i++)
+	{
+		Num1 = 0;
+		srand(time(NULL)*i);
+		Num1 = (rand() % 52);
+		while (Num1 != Array[i])
+		{
+			Num1 = 0;
+			srand(time(NULL) * i);
+			Num1 = (rand() % 52);
+			Array[i] = Num1;
+		}
+		cout << Array[i]<<endl;
 	}
 	cout << endl;
-	//사용하지 않고 표준편차를 이용.
-	for (int i = 0; i < 4; i++) {
 
-		random_device rd; 
-		mt19937 gen(rd());
-		uniform_int_distribution<int> Num2(0, 52);
-		cout << Num2(gen) << endl;
+
+	//사용하지 않고 표준편차를 이용.
+	//현재 문제가 있음.
+	/*
+	int Array2[4] = { 0,0,0,0 };
+	int Num2 = 0;
+	uniform_int_distribution<int> NumDis(0, 52);
+	random_device rd;
+	mt19937 gen(rd());
+	for (int i = 0; i < 4; i++)
+	{
+		Num2 = NumDis(gen);
+		while (Num2 != Array2[i])
+		{
+			Num2 = NumDis(gen);
+			Array2[i] = Num2;
+		}
+		cout << Array2[i] << endl;
+
+
 	}
 
+	*/
 
 	return 0;
 }
